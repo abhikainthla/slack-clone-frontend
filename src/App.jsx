5380951a7e16ae78e610e7e2c7a9d5fb4d5388ac
Abhishek Kainthla <105478999+abhikainthla@users.noreply.github.com>
2026-03-14 15:34:17 +0530

    Added basic setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-purple-600">
+        Slack Clone Frontend 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
